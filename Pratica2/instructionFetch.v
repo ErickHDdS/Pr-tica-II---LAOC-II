57d9dea91c9a481ecc7e121e9d57ae76dc524fd1
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-07-22 18:00:58 -0300

    Update instructionFetch.v

--- Pratica2/instructionFetch.v
+++ Pratica2/instructionFetch.v
@@ -6,7 +6,7 @@ module instructionFetch(addr, MClock, DIN);
 	
 	wire [5:0] memOut;
 		
-	romlpm address(addr, MClock, memOut);  	// Gera uma pagina virtual, tera que passar uma TLB	
-	TLB tlb(memOut, MClock, DIN);					// Endereco pagina fisica = tlbOut
+	romlpm address(addr, MClock, memOut);  	
+	TLB tlb(memOut, MClock, DIN);			
 	
 endmodule
