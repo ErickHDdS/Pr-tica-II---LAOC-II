bd138102967dccd6eeab2558d33f676401b057fa
Marina <62568976+pixel-debug@users.noreply.github.com>
2021-07-22 17:45:33 -0300

    Update proc.v

--- Pratica2/proc.v
+++ Pratica2/proc.v
@@ -1,8 +1,4 @@
 module proc (DIN, mem, Resetn, Clock, Run, Done, BusWires, R7, addrM, doutM, wM);
-	// [15:12] = Instruction 
-	// [11:9] = Xreg 
-	// [8:6] = Yreg 
-	// [5:0] = Offset
 	input [15:0] DIN; 
 	input [15:0] mem;
 	input Resetn, Clock, Run;
@@ -14,30 +10,39 @@ module proc (DIN, mem, Resetn, Clock, Run, Done, BusWires, R7, addrM, doutM, wM)
 	output [15:0] addrM, doutM;
 	output wM;
 	
-	//... declare variables
-		wire Clear = Done | Resetn;		
-		// regs
-			wire [0:7] Xreg, Yreg;
-			wire [15:0] R0, R1, R2, R3, R4, R5, R6, A, G;	
-			wire [15:0] aluOut;
-		// instructions
-			wire [9:0] IR;
-			wire [3:0] I;	
-		// time steps
-			wire [1:0] TstepQ;		
-		// signals
-			reg dinOut, gOut, memOut, incr_pc, aIn, gIn, irIn, addrIn, doutIn, wIn;
-			reg [0:7] regOut, regIn;
-			reg [2:0] aluSignal;
-		
+	// registradores
+	wire [0:7] Xreg, Yreg;
+	wire [15:0] R0, R1, R2, R3, R4, R5, R6, A, G;	
+	wire [15:0] aluOut;
+	
+	// instructions
+	wire [9:0] IR;
+	wire [3:0] I;	
+	
+	// time steps
+	wire [1:0] TstepQ;		
+	
+	// sinais
+	reg dinOut, gOut, memOut, incr_pc, aIn, gIn, irIn, addrIn, doutIn, wIn;
+	reg [0:7] regOut, regIn;
+	reg [2:0] aluSignal;
+	
+	wire Clear = Done | Resetn;	
+	
+	// [15:12] = Instruction 
+	// [11:9] = Xreg 
+	// [8:6] = Yreg 
+	// [5:0] = Offset
+	
 	assign I = IR[9:6];
+	// extensão de sinal
 	dec3to8 decX (IR[5:3], 1'b1, Xreg);
 	dec3to8 decY (IR[2:0], 1'b1, Yreg);	
+	
 	upcount tstep (Clear, Clock, TstepQ);
 
 	always @(TstepQ or I or Xreg or Yreg) begin
 		
-		//... specify initial values		
 		regOut[0:7] = 8'b0;
 		regIn [0:7] = 8'b0;	
 		dinOut = 1'b0;			
@@ -89,11 +94,7 @@ module proc (DIN, mem, Resetn, Clock, Run, Done, BusWires, R7, addrM, doutM, wM)
 						end
 						
 						4'b0100: begin					//mvi					
-							//FOI STAll
-							//regIn = Xreg;
-							//dinOut = 1;
-							//Done = 1;
-							//incr_pc = 1;
+							
 						end
 						
 						
@@ -148,13 +149,7 @@ module proc (DIN, mem, Resetn, Clock, Run, Done, BusWires, R7, addrM, doutM, wM)
 							addrIn = 1;
 							wIn = 1;
 						end
-						
-						//if(I == 4'b0011) begin 	//ld
-						//	dinOut = 1'b1;
-						//	regIn = Xreg;
-						//	Done = 1;
-						//end
-						
+												
 						if(I == 4'b0100) begin		//mvi
 							regIn = Xreg;
 							dinOut = 1'b1;
@@ -181,38 +176,35 @@ module proc (DIN, mem, Resetn, Clock, Run, Done, BusWires, R7, addrM, doutM, wM)
 		end
 	end
 		
-	//... instantiate other registers and the adder/subtracter unit
-		// Standart registers
-			regn reg_0 (BusWires, regIn[0], Clock, R0, Resetn);
-			regn reg_1 (BusWires, regIn[1], Clock, R1, Resetn);
-			regn reg_2 (BusWires, regIn[2], Clock, R2, Resetn);
-			regn reg_3 (BusWires, regIn[3], Clock, R3, Resetn);
-			regn reg_4 (BusWires, regIn[4], Clock, R4, Resetn);
-			regn reg_5 (BusWires, regIn[5], Clock, R5, Resetn);
-			regn reg_6 (BusWires, regIn[6], Clock, R6, Resetn);
-						
-		//Counter
-			counterlpm reg_7 (1'b1, Clock, incr_pc, BusWires[5:0], Resetn, regIn[7], R7);
-			
-		//Alu
-			ULA ula(aluSignal, A, BusWires, aluOut);
-			
-		// Aux reg for multi-cycle
-			regn reg_A(BusWires, aIn, Clock, A, Resetn);
-			regn reg_G(aluOut, gIn, Clock, G, Resetn);			
-			
-		// Instruction register
-			regn reg_IR(DIN[15:6], irIn, Clock, IR, Resetn);	
-			defparam reg_IR.n = 10; 
-			
-		// Memory register
-			regn reg_Addr(BusWires, addrIn, Clock, addrM, Resetn);
-			regn reg_Dout(BusWires, doutIn, Clock, doutM, Resetn);		
-			regn reg_W(wIn, 1'b1, Clock, wM, Resetn);	
-			defparam reg_W.n = 1;	
+	//... instantiate other registers
+	
+	regn reg_0 (BusWires, regIn[0], Clock, R0, Resetn);
+	regn reg_1 (BusWires, regIn[1], Clock, R1, Resetn);
+	regn reg_2 (BusWires, regIn[2], Clock, R2, Resetn);
+	regn reg_3 (BusWires, regIn[3], Clock, R3, Resetn);
+	regn reg_4 (BusWires, regIn[4], Clock, R4, Resetn);
+	regn reg_5 (BusWires, regIn[5], Clock, R5, Resetn);
+	regn reg_6 (BusWires, regIn[6], Clock, R6, Resetn);
+	
+	counterlpm reg_7 (1'b1, Clock, incr_pc, BusWires[5:0], Resetn, regIn[7], R7);		// é um counter
+	
+	regn reg_A(BusWires, aIn, Clock, A, Resetn);
+	regn reg_G(aluOut, gIn, Clock, G, Resetn);
+	
+	regn reg_IR(DIN[15:6], irIn, Clock, IR, Resetn);	
+	defparam reg_IR.n = 10; // instancia a n com 10 
+	
+	ULA ula(aluSignal, A, BusWires, aluOut);
+	
+	
+	// Memory register
+	regn reg_Addr(BusWires, addrIn, Clock, addrM, Resetn);
+	regn reg_Dout(BusWires, doutIn, Clock, doutM, Resetn);		
+	regn reg_W(wIn, 1'b1, Clock, wM, Resetn);	
+	defparam reg_W.n = 1;	// instancia a n com 1
 						
 	//... define the bus
-			MUX mux (DIN, R0, R1, R2, R3, R4, R5, R6, R7, G, mem, {dinOut, regOut, gOut, memOut}, BusWires);
+	MUX mux (DIN, R0, R1, R2, R3, R4, R5, R6, R7, G, mem, {dinOut, regOut, gOut, memOut}, BusWires);
 	
 	
 	
